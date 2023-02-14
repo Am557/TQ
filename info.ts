dbf0a0020f48352ab082df70173b83084bd224dd
Am557 <125130481+Am557@users.noreply.github.com>
2023-02-14 21:49:46 +0530

    Add files via upload

---
+++ info.ts
@@ -0,0 +1,11 @@
+export class Info{
+    constructor(
+        public name:string,
+        public email:string,
+        public pwd:string,
+        public repwd:string,
+        public gender:string,
+        public dob:any,
+        public phone:string
+    ){}
+}
